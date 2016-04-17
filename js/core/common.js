32a662a61a10d0039a955e6aeab861308a3944fe
Taras Kolodny <trskldn@gmail.com>
2016-04-17 15:20:22 +0300

    region class

---
+++ js/core/common.js
@@ -0,0 +1,18 @@
+define(['Backbone'], function (Backbone) {
+  function Region() {
+
+  }
+  Region.prototype.show = function () {
+
+  };
+
+  Region.prototype.close = function () {
+
+  };
+
+  Region.prototype.show = function () {
+
+  };
+
+  return {};
+});
