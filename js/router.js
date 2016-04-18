f491f1c0db8ceb827c756d0935776f019b5b06c3
Taras Kolodny <trskldn@gmail.com>
2016-04-18 20:24:30 +0300

    sub module loading

--- js/router.js
+++ js/router.js
@@ -1,32 +1,56 @@
-define(['Backbone'], function( Backbone ) {
+define([
+'Backbone',
+// 'modules/chat/router'
+], function( Backbone ) {
 
 	return Backbone.Router.extend({
 		routes: {
-			//  '*': 'defaultAction',
-			'room/:id': 'switchRoom',
-			'login': 'login',
-			'logout': 'logout'
+			 '': 'defaultRoute',
+			 // 'home': 'testhomePage',
+ 			// '*mod/*rest': 'loadModule',
+			'*module(/*hash)': 'loadModule'		
 		},
 
-		initialize: function () {
-				require(['views/MainView'], function(MainView) {
-					new MainView({el: '#main'}).render();
-				});
+		initialize: function (options) {
+			// require(['views/MainView'], function(MainView) {
+			// 	window.MainView = MainView;
+			// 	new MainView({el: '#main'}).render();
+			// });
+			// this.currentApp = void 0;
+			this._modules = {};
+			this.options = options;
 		},
 
-		defaultAction: function() {
+		testhomePage: function() {
+			console.log('home router from main router')
+			return false;
 		},
 
-		switchRoom: function (roomId) {
-
+		defaultRoute: function() {
+	    	Backbone.history.navigate("/home", true);
 		},
 
-		login: function () {
+		loadModule: function(moduleName, hash) {
+			console.log('load ', moduleName);
+			var self = this;
 
-		},
+			require(['modules/'+ moduleName+'/app'], function(Module) {
+				if (!self._modules[moduleName]) {
+					console.log(moduleName, 'loaded');
+					var module = new Module({mainRegion: self.options.mainRegion});
+					module.start();
+					self._modules[moduleName] = module;
+				} else {
+					console.log(moduleName,' already loaded');
+				}
+				// if (self.currentApp instanceof app) {
 
-		logout: function () {
-
+				// }
+			},function(error) {
+				console.log('error loaded ',arguments);
+		    	Backbone.history.navigate("/");
+			});
+			// return false;
 		}
 	});
 });
