b6193abe6190251d42e110befda9738fd3e82976
Taras Kolodny <trskldn@gmail.com>
2016-04-20 12:59:11 +0300

    navigation avtive item highlight

--- js/shared/views/NavHeaderView.js
+++ js/shared/views/NavHeaderView.js
@@ -17,7 +17,13 @@ function (Backbone, Common, NavHeaderViewTmpl) {
 
   	_changeRoute: function() {
 			console.log('fragment:',Backbone.history.fragment);	
-			window.nav = this.$el.find('.navbar-nav  a');
+			this.$el.find('.navbar-nav  a').each(function(i, el) {
+				if (!('#'+Backbone.history.fragment).indexOf(el.getAttribute('href')) && el.getAttribute('href').length > 1){
+					$(el.parentNode).toggleClass('active', true);
+				} else {
+					$(el.parentNode).removeClass('active');
+				}
+			});
   	}
   });
   
