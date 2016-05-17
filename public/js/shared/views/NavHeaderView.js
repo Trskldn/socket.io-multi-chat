793730aad3e6a6abb8989d9b8b20e80a69b0d163
Taras Kolodny <trskldn@gmail.com>
2016-05-17 23:48:20 +0300

    authentication and jwt

--- public/js/shared/views/NavHeaderView.js
+++ public/js/shared/views/NavHeaderView.js
@@ -1,31 +1,52 @@
 define([
-'backbone', 
-'core/common', 
-'text!./../templates/NavHeaderView.html'],
-function (Backbone, Common, NavHeaderViewTmpl) {
-  var NavHeaderView = Common.ModelView.extend({
-  	
-  	template: NavHeaderViewTmpl,
-  	
-  	tagName:'div',
-  	
-  	className: 'container',
-  	
-  	initialize: function() {
-      Common.ModelView.prototype.initialize.apply(this, arguments);
-	   	this.listenTo(Backbone.history,'route', this._changeRoute);
-  	},
+    'backbone',
+    'core/common',
+    'text!./../templates/NavHeaderView.html'
+  ],
+  function(Backbone, Common, NavHeaderViewTmpl) {
+    var NavHeaderView = Common.ModelView.extend({
 
-  	_changeRoute: function() {
-			this.$el.find('.navbar-nav  a').each(function(i, el) {
-				if (!('#'+Backbone.history.fragment).indexOf(el.getAttribute('href')) && el.getAttribute('href').length > 1){
-					$(el.parentNode).toggleClass('active', true);
-				} else {
-					$(el.parentNode).removeClass('active');
-				}
-			});
-  	}
+      template: NavHeaderViewTmpl,
+
+      tagName: 'div',
+
+      className: 'container',
+
+      initialize: function() {
+        Common.ModelView.prototype.initialize.apply(this, arguments);
+        this.listenTo(Backbone.history, 'route', this._changeRoute);
+        this.listenTo(app.session, 'change:isLogged', this._onAuthChange);
+      },
+
+      _onAuthChange: function() {
+        app.session
+        this.$el.find('a[data-auth]').each(function(i, el) {
+          console.log();
+          var $el = $(el);
+
+          $el.toggleClass('hide', app.session.get('isLogged') != $el.data('auth'));
+        });
+      },
+
+      render: function() {
+        this.inherited('render', arguments);;
+        this._onAuthChange();
+      },
+
+      _changeRoute: function() {
+        this.highlightMenuItem();
+      },
+
+      highlightMenuItem: function() {
+        this.$el.find('.navbar-nav a').each(function(i, el) {
+          if (!('#' + Backbone.history.fragment).indexOf(el.getAttribute('href')) && el.getAttribute('href').length > 1) {
+            $(el.parentNode).toggleClass('active', true);
+          } else {
+            $(el.parentNode).removeClass('active');
+          }
+        });
+      }
+    });
+
+    return NavHeaderView;
   });
-  
-  return NavHeaderView;
-});
